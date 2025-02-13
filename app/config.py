f667ec64dfcdf199066e9bfd9be004d1fde4c590
muhilan-a <121476496+muhilan-a@users.noreply.github.com>
2025-02-13 15:31:34 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,5 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'cves.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
